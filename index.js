4243e69d232100a8ab36fe4840e885ae3f38bd40
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-09-24 18:14:40 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+let multiply ,num1 = 31 ,num2 = 2,random ,mod ,num3 = 20 ,num4 = 16 ,max ;
 
-// Write your code here
+multiply = num1 * num2;
+random = Math.ceil(Math.random());
+mod = num3 % num4;
+max = Math.max( 5, 8, 16, 20);
